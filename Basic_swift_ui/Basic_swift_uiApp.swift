68e2daec47f3278ae6097944d7b692af32d738a0
HungLD <hungldgch29091996@gmail.com>
2023-02-17 18:02:32 +0700

    push code

---
+++ Basic_swift_ui/Basic_swift_uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Basic_swift_uiApp.swift
+//  Basic_swift_ui
+//
+//  Created by HungLD on 17/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct Basic_swift_uiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
